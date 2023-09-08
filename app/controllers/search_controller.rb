1f9545c745df7819ad6be5c55d4af43bf3256968
Davis Weimer <davisweimer@gmail.com>
2023-09-08 09:53:31 -0600

    vcr added + nation_decoder

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,24 @@
 class SearchController < ApplicationController
   def index
+    require 'pry'; binding.pry
+    @nation = nation_decoder(params[:nation])
     @characters = SearchFacade.get_all_characters
   end
+
+  private
+  def nation_decoder(nation_params)
+    nations = {
+      "fire+nation" => "Fire Nation",
+      "air+nomads" => "Air Nomads",
+      "earth+kingdom" => "Earth Kingdom",
+      "water+tribe" => "Water Tribe"
+    }
+    
+    nations[nation_params] || nation_params
+  end
 end
+
+# "fire+nation"
+# "air+nomads"
+# "earth+kingdoms"
+# "water+tribes"
--- spec/features/welcome/nation_search_spec.rb
+++ spec/features/welcome/nation_search_spec.rb
@@ -2,17 +2,16 @@ require "rails_helper"
 
 RSpec.describe "Avatar API", type: :feature do
   describe "#GET" do
-    it "can select from a list of nations" do
+    it "can select from a list of nations", :vcr do
       visit root_path
 
       expect(page).to have_content("The Four Nations")
 
-      select "Fire Nation", from: "nation"
+      select "Air Nomads", from: "nation"
       click_button "Search For Members"
 
       expect(current_path).to eq(search_path)
-
-      
+      expect(page).to have_content("Fire Nation")
     end
   end
 end
